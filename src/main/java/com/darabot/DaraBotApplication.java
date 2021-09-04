f8372a13f0055bba780d0e05077478a00af2bd11
Juyoung Bahng <qkdwn0313@gmail.com>
2021-09-04 14:47:29 +0900

    Initial commit

---
+++ src/main/java/com/darabot/DaraBotApplication.java
@@ -0,0 +1,13 @@
+package com.darabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaraBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaraBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darabot/DaraBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaraBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
